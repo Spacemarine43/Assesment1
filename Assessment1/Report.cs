4ea1636963a82d5c4c947debf479e5eb4a1d89ae
Spacemarine43 <101358560+Spacemarine43@users.noreply.github.com>
2022-03-31 15:02:05 +0100

    Update Report.cs

--- Assessment1/Report.cs
+++ Assessment1/Report.cs
@@ -10,9 +10,9 @@ namespace Assessment1
     {
         public void Output(List<int> analysedText)
         {
-            Console.WriteLine("The number of sentences is: " + analysedText[0]);
-            Console.WriteLine("The number of vowels is: " + analysedText[1]);
-            Console.WriteLine("The number of consonants is: " + analysedText[2]);
+            Console.WriteLine("The number of sentences is: " + analysedText[0]);//utputs number of sentences
+            Console.WriteLine("The number of vowels is: " + analysedText[1]);//Outputs number of vowels
+            Console.WriteLine("The number of consonants is: " + analysedText[2]);//Outputs number of consonants
             Console.WriteLine("The number of uppercase letters is: " + analysedText[3]);
             Console.WriteLine("The number of lower case letters is: " + analysedText[4]);
         }
